dcc6c7a92e5d6c3615a78e49283996771f628fe7
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-08-05 12:29:34 +0100

    organisation of the main page to many component

--- frontend/src/app/components/resume-profiles/resume-profiles.module.ts
+++ frontend/src/app/components/resume-profiles/resume-profiles.module.ts
@@ -4,18 +4,18 @@ import { RouterModule } from '@angular/router';
 
 import { ResumeProfilesRoutingModule } from './resume-profiles-routing.module';
 import { MainProfileComponent } from './main-profile/main-profile.component';
-import { SuggestionComponent } from './suggestion/suggestion.component';
-import { TestComponent } from './test/test.component';
+
+
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [MainProfileComponent, SuggestionComponent, TestComponent],
+  declarations: [MainProfileComponent,],
   imports: [ SharedModule, RouterModule],
   exports: [
     MainProfileComponent,
-    SuggestionComponent,
+
     ResumeProfilesRoutingModule,
   ],
 })
