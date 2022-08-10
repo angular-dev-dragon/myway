8a0b800cbbb309b8d9d242f8b5e02041f874218e
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-08-10 10:44:24 +0100

    add

--- frontend/src/app/components/resume-profiles/side-bar/head-profile/head-profile.component.ts
+++ frontend/src/app/components/resume-profiles/side-bar/head-profile/head-profile.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
 @Component({
   selector: 'Head-Profile',
   templateUrl: './head-profile.component.html',
   styleUrls: ['./head-profile.component.scss', '../../style/main.scss'],
 })
 export class HeadProfileComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(
 
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-  }
+    private primengConfig: PrimeNGConfig,
+    private messageService: MessageService
+
+  ){}
 
   hero: boolean | undefined;
 
   showhero() {
     this.hero = true;
+  }
+
+
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
