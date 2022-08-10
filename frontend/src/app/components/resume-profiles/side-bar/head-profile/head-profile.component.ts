aebd90c1f44af85b316ca7d2e6d87eecec6c835c
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-08-10 18:04:50 +0100

    ADD FROM VALIDATION

--- frontend/src/app/components/resume-profiles/side-bar/head-profile/head-profile.component.ts
+++ frontend/src/app/components/resume-profiles/side-bar/head-profile/head-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { NgbModal, NgbModalConfig, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder, Validators , FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'Head-Profile',
@@ -7,22 +9,76 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./head-profile.component.scss', '../../style/main.scss'],
 })
 export class HeadProfileComponent implements OnInit {
+  date: any;
+  public entretienForm: FormGroup;
   constructor(
+    private c: NgbModalConfig,
+    private ms: NgbModal,
+    private fb: FormBuilder
+  ) {
 
-    private primengConfig: PrimeNGConfig,
+    this.entretienForm = this.fb.group({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+        Validators.pattern('^[a-zA-Z \u0600-\u06FF]+$'),
+      ]),
+      prenom: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+        Validators.pattern('^[a-zA-Z \u0600-\u06FF]+$'),
+      ]),
 
+      telephone: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^((\\+212-?)|0)?[0-9]{10}$'),
+        Validators.minLength(10),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
 
-  ){}
+      // noteEntretien: new FormControl('', [Validators.required]),
+      // difficulte: new FormControl('', [Validators.required]),
+      // recommandation: new FormControl('', [Validators.required]),
 
-  hero: boolean | undefined;
+      // note: new FormControl('', [Validators.required]),
+      // salaire: new FormControl('', [Validators.required]),
+      // securite: new FormControl('', [Validators.required]),
+    });
+  }
+  ngOnInit() {}
 
-  showhero() {
-    this.hero = true;
+  open(content: any) {
+    this.ms.open(content, { centered: true, size: 'xl' });
+  }
+
+  sendEntretien() {
+    if (this.entretienForm.valid) {
+      console.log('this.entretienForm', this.entretienForm);
+    } else {
+      this.entretienForm.markAllAsTouched();
+      console.log('not valid');
+
+    }
   }
 
 
 
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-  }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
