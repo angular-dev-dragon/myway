8712f8bba83b5b0002d3281fe1408f5c937b2f2a
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-08-19 19:23:07 +0100

    add

--- frontend/src/app/components/landing-page/landing-page.module.ts
+++ frontend/src/app/components/landing-page/landing-page.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LayoutsComponent } from './layouts/layouts.component';
+import { NgModule } from '@angular/core';
+
+
+
+
 
 
 @NgModule({
-  declarations: [
-    LayoutsComponent
-  ],
+  
+  declarations: [LayoutsComponent],
+
   imports: [
+
+    SharedModule,
     CommonModule,
-    LandingPageRoutingModule
-  ]
+
+    LandingPageRoutingModule,
+
+  ],
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
