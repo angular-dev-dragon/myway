043271a1809085bd874a0e6d7697a62ededd99f5
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-08-24 19:06:10 +0100

    add all comipemns  card

--- frontend/src/app/shared/Services/slider/slider.service.ts
+++ frontend/src/app/shared/Services/slider/slider.service.ts
@@ -66,10 +66,10 @@ export class SliderService {
             },
           },
           {
-            name: 'footer',
-            id: '223kd',
-            drtails: {
-              width: '100%',
+            name: 'test',
+            id: '14',
+            details: {
+              width: '50%',
               height: '85vh',
               time_btwn_slide: 5000,
               Arrows_btns: true,
@@ -77,6 +77,27 @@ export class SliderService {
               footer: false,
               animation: true,
               info: [
+                {
+                  url: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+                  alt: '1',
+                  title: 'Slider 1',
+                  p: 'heder 1 text',
+                  id: '1',
+                },
+                {
+                  url: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+                  alt: '2',
+                  title: 'Slider3',
+                  p: 'heder 1 text',
+                  id: '2',
+                },
+                {
+                  url: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
+                  alt: '3',
+                  title: 'Slider 2',
+                  p: 'sheder 2 text',
+                  id: '3',
+                },
                 {
                   url: 'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg',
                   alt: '1',
--- frontend/src/app/shared/components/card-library/card-library.component.ts
+++ frontend/src/app/shared/components/card-library/card-library.component.ts
@@ -6,8 +6,20 @@ import { Component, Input, OnInit } from '@angular/core'
   styleUrls: ['./card-library.component.scss'],
 })
 export class CardLibraryComponent implements OnInit {
-  @Input() info: any
-
+  @Input() info: any;
+  @Input() imageBg = '';
+  @Input() logo = '';
+  @Input() title    = '';
+  @Input() type      = '';
+  @Input() salaries  = '';
+  @Input() caption   = '';
+  getPosition(a1: any, a2: any) {
+    for (let i of this.info) {
+      a1.Object.key(i.p.span)[0];
+      a2.Object.key(i.p.text);
+      console.log(a1);
+    }
+  }
   constructor() {}
 
   ngOnInit(): void {}
