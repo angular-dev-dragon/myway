3949b7250e13dbf8ad019ee88a2148a261bbe6da
Hicham Berdouki <79708893+HiichamB@users.noreply.github.com>
2022-08-29 16:42:27 +0100

    quiz version Finale
    Update design
    Add Statiqtique

--- frontend/src/app/components/quiz/list-quiz/list-quiz.component.ts
+++ frontend/src/app/components/quiz/list-quiz/list-quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core'
 
 @Component({
   selector: 'app-list-quiz',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./list-quiz.component.scss'],
 })
 export class ListQuizComponent implements OnInit {
-  constructor() {}
+  constructor() {
+    this.listQuiz = this.allListQuiz
+  }
 
   ngOnInit(): void {}
-  listQuiz: any = [
+  allListQuiz: any = [
     {
       image:
         'https://wegate.eu/sites/default/files/styles/resize_750x450/public/coursera.jpg?itok=5dhpuGwQ',
@@ -22,6 +24,8 @@ export class ListQuizComponent implements OnInit {
       date: '2022-05-15',
       niveau: 'Avancé',
       note: 5,
+      secteur: 'Informatique',
+      metier: ['Developpeur', 'Developpeur Front-End'],
     },
     {
       image:
@@ -36,6 +40,8 @@ export class ListQuizComponent implements OnInit {
       date: '2022-05-15',
       niveau: 'Avancé',
       note: 4.5,
+      secteur: 'Informatique',
+      metier: ['Developpeur'],
     },
     {
       image:
@@ -50,6 +56,8 @@ export class ListQuizComponent implements OnInit {
       date: '2022-05-15',
       niveau: 'Débutant',
       note: 3.5,
+      secteur: 'Agriculture',
+      metier: ['DevOps'],
     },
     {
       image:
@@ -64,6 +72,88 @@ export class ListQuizComponent implements OnInit {
       nbQuestions: 10,
       date: '2022-05-15',
       note: 4,
+      secteur: 'Électronique',
+      metier: ['DevOps'],
     },
   ]
+  listQuiz: any = []
+
+  @ViewChildren('metier') metierBox!: any
+
+  @ViewChild('secteur') secteurSelect!: any
+
+  @ViewChild('search') searchInput!: any
+
+  filter() {
+    this.listQuiz = this.allListQuiz
+
+    let secteurFiltre = this.secteurSelect.nativeElement.value
+    console.log('filtre par secteurs', secteurFiltre)
+
+    let searchFiltre = this.searchInput.nativeElement.value
+
+    let metierFiltre = this.metierBox._results
+    // console.log('filtre par contrat', contratFiltre)
+
+    // console.log('filtre par date', dateFiltre)
+    //let metierFilter = this.metierSelect.nativeElement.value
+
+    if (searchFiltre != '') {
+      this.listQuiz = this.listQuiz.filter((recruteur: any) => {
+        return recruteur.title
+          .toLowerCase()
+          .includes(searchFiltre.toLowerCase())
+      })
+    }
+
+    if (secteurFiltre != '') {
+      this.listQuiz = this.listQuiz.filter((recruteur: any) => {
+        return recruteur.secteur == secteurFiltre
+      })
+    }
+    this.checkboxFiltre(metierFiltre, 'metier')
+  }
+
+  checkboxFiltre(CheckboxList: any, label: any) {
+    let newList2
+    let newList3: any = this.listQuiz
+    let isFirstTime: Boolean = true
+    CheckboxList.map((filtre: any) => {
+      if (filtre.nativeElement.checked) {
+        if (isFirstTime == true) {
+          isFirstTime = false
+          newList3 = []
+        }
+
+        newList2 = this.listQuiz
+        newList2 = newList2.filter((quiz: any) => {
+          return quiz.metier.includes(filtre.nativeElement.value)
+        })
+
+        newList2.map((list: any) => {
+          newList3.push(list)
+        })
+      }
+    })
+    this.listQuiz = newList3
+    console.log(
+      '🚀 ~ file: list-quiz.component.ts ~ line 140 ~ ListQuizComponent ~ checkboxFiltre ~   this.listQuiz',
+      this.listQuiz,
+    )
+  }
+
+  sort(type: any) {
+    this.listQuiz.sort((a: any, b: any) =>
+      a[type].toLowerCase() > b[type].toLowerCase() ? 1 : -1,
+    )
+  }
+  sortbyDate() {
+    this.listQuiz.sort((a: any, b: any) => (a.Date < b.Date ? 1 : -1))
+  }
+  showSidebar: boolean = false
+
+  showFilter() {
+    this.showSidebar = !this.showSidebar
+  }
+  sideBarValue: string = 'ads'
 }
--- frontend/src/app/components/quiz/quiz-details/quiz-details.component.ts
+++ frontend/src/app/components/quiz/quiz-details/quiz-details.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core'
 export class QuizDetailsComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.calculStatistique(this.quiz)
+  }
   quiz: any = {
     image:
       'https://wegate.eu/sites/default/files/styles/resize_750x450/public/coursera.jpg?itok=5dhpuGwQ',
@@ -21,7 +23,16 @@ export class QuizDetailsComponent implements OnInit {
     date: '2022-05-15',
     niveau: 'Avancé',
     note: 5,
-
+    translations: {
+      fr: {
+        __titre: 'Quiz HTML CSS',
+        __nom_responsable: 'fdsfsfs',
+        __description:
+          "Consequat NesciuntUne organisation est en sciences sociales un groupe social formé d'individus en interaction, ayant un but collectif, mais dont les préférences, les informations, les intérêts et les connaissances peuvent diverger1 : une entreprise, une administration publique, un syndicat, un parti politique, une association, etc.\n\nL'organisation de quelque chose désigne l'action d'organiser (structurer, délimiter, agencer, répartir ou articuler). En ce sens, il s'agit d'un processus social.\n\nUne organisation est le résultat d'actions réglementées (une entreprise, un service public, une administration, une association, une armée, un événement.).",
+      },
+      ar: { __designation: '', __nom_responsable: '', __description: '' },
+      en: { __designation: '', __nom_responsable: '', __description: '' },
+    },
     details: [
       {
         description:
@@ -34,5 +45,244 @@ export class QuizDetailsComponent implements OnInit {
           'Le projet est une forme d’organisation des affaires collectives apparue dans l’après Seconde Guerre mondiale pour rationaliser les grandes entreprises du complexe militaro-industriel américain (Ika et Hodgson, 2014). Il a rapidement gagné de nombreux secteurs d’activité, constituant notamment l’un des instruments principaux de l’action publique dans les pays du Sud (Hirschman, 1967). En première instance, un projet de développement peut être défini comme une organisation temporaire, dotée de moyens dédiés – humains, financiers, techniques – fonctionnant comme un dispositif à double détente : sa mise en œuvre vise des objectifs propres et est censée servir de levier de transformation de l’action publique à plus grande échelle (Sjöblom et al., 2013). Si une littérature importante a considéré le projet de développement comme unité d’analyse privilégiée pour questionner son efficacité au regard de ses objectifs (e.g. Castellanet, 2003) et ses effets sur les dynamiques sociales locales (e.g. Olivier de Sardan, 1995), les modalités de son articulation à l’action publique ont été moins analysées. Cet article propose ainsi de mettre en regard (i) la question de l’efficacité des projets et (ii) leur capacité à faire évoluer la conduite de l’action publique.',
       },
     ],
+    ref_quiz: 'refML001254785',
+    date_ouverture: '2022-02-03',
+    date_cloture: '2022-02-03',
+
+    nombre_questions: null,
+    statistique_score: [
+      {
+        _id: '6256aef016902822ac894323',
+        N_Reponses_Correctes: 0,
+        Nombre_N_Reponses_Justes: 6,
+        Classement_Participation: 8,
+      },
+      {
+        _id: '6256aef016902822ac894324',
+        N_Reponses_Correctes: 1,
+        Nombre_N_Reponses_Justes: 2,
+        Classement_Participation: 2,
+      },
+      {
+        _id: '6256aef016902822ac894325',
+        N_Reponses_Correctes: 2,
+        Nombre_N_Reponses_Justes: 0,
+        Classement_Participation: 0,
+      },
+    ],
+
+    participation_quiz: [
+      {
+        _id: { $oid: '625938d6cefa5c161c8dc2f2' },
+        score_obtenu: '1',
+        note_obtenue: '0.5',
+      },
+      {
+        _id: { $oid: '6259394bcefa5c161c8dc2f6' },
+        score_obtenu: '0',
+        note_obtenue: '0',
+      },
+      {
+        _id: { $oid: '62593dbecefa5c161c8dc322' },
+        score_obtenu: '0',
+        note_obtenue: '0',
+      },
+      {
+        _id: { $oid: '625d93b17d9de5eb4b15f24f' },
+        score_obtenu: '0',
+        note_obtenue: '0',
+      },
+      {
+        _id: { $oid: '625d94307d9de5eb4b15f253' },
+        score_obtenu: '0',
+        note_obtenue: '0',
+      },
+      {
+        _id: { $oid: '625fe07678cb1361a3cd9131' },
+        score_obtenu: '0',
+        note_obtenue: '0',
+      },
+      {
+        _id: { $oid: '62e2ad36125d8d57290a97b3' },
+        score_obtenu: '0',
+        note_obtenue: '0',
+      },
+      {
+        _id: { $oid: '6308f271fe8f1fc7911c7cf6' },
+        score_obtenu: '1',
+        note_obtenue: '0.5',
+      },
+    ],
+
+    question_quiz: [
+      {
+        _id: '6256aedf16902822ac894319',
+        translations: {
+          fr: {
+            __label_question: 'Que signifie HTML ?',
+            __indicateur: 'Wilkerson Ward Co',
+          },
+          ar: { __label_question: '', __indicateur: '' },
+          en: { __label_question: '', __indicateur: '' },
+        },
+        valeur_evaluation: '0',
+        ordre: '',
+        etat_objet: 'active',
+        etat_validation: '',
+        type: 'simple',
+        reponse_quiz: [
+          {
+            translations: {
+              fr: { __texte_reponse: ' Home Tool Markup Language' },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aedf16902822ac89431a',
+            __valeur_reponse_juste: true,
+          },
+          {
+            translations: {
+              fr: {
+                __texte_reponse: ' Hyperlinks and Text Markup Language',
+              },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aedf16902822ac89431b',
+            __valeur_reponse_juste: false,
+          },
+          {
+            translations: {
+              fr: { __texte_reponse: ' Hyper Text Markup Language' },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aedf16902822ac89431c',
+            __valeur_reponse_juste: false,
+          },
+        ],
+        image:
+          'http://localhost:3600/uploads/images/quiz/image-1649848031269.jpg',
+      },
+      {
+        _id: '6256aee816902822ac89431d',
+        translations: {
+          fr: {
+            __label_question:
+              'Parmi les balises suivantes, lesquelles sont obligatoires par le standard HTML ?',
+            __indicateur: 'Schultz and Hoffman LLC',
+          },
+          ar: { __label_question: '', __indicateur: '' },
+          en: { __label_question: '', __indicateur: '' },
+        },
+        valeur_evaluation: '0',
+        ordre: '',
+        etat_objet: 'active',
+        etat_validation: '',
+        type: 'multiple',
+
+        reponse_quiz: [
+          {
+            translations: {
+              fr: { __texte_reponse: '<html>' },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aee816902822ac89431e',
+            __valeur_reponse_juste: true,
+          },
+          {
+            translations: {
+              fr: { __texte_reponse: '<head>' },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aee816902822ac89431e',
+            __valeur_reponse_juste: false,
+          },
+          {
+            translations: {
+              fr: { __texte_reponse: '<script>' },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aee816902822ac89431e',
+            __valeur_reponse_juste: false,
+          },
+          {
+            translations: {
+              fr: { __texte_reponse: '<body>' },
+              ar: { __texte_reponse: '' },
+              en: { __texte_reponse: '' },
+            },
+            _id: '6256aee816902822ac89431e',
+            __valeur_reponse_juste: true,
+          },
+        ],
+        image:
+          'http://localhost:3600/uploads/images/quiz/image-1649848040085.webp',
+      },
+    ],
+  }
+  statistique_score: any = []
+  statistique_qst: any = []
+  table_question: any = []
+
+  scores: any = {}
+  notes: any = {}
+  nb_participant = 0
+  calculStatistique(success: any) {
+    this.nb_participant = success.participation_quiz.length || 0
+    if (success && success.participation_quiz && this.nb_participant) {
+      this.statistique_score = success.statistique_score
+      var scores = success.participation_quiz.map(
+        (elt: any) => elt.score_obtenu,
+      )
+      var notes = success.participation_quiz.map((elt: any) => elt.note_obtenue)
+
+      var i = 0,
+        j = 0,
+        summScore = 0,
+        summNotes = 0
+      while (i < notes.length) {
+        summNotes = summNotes + Number(notes[i++])
+      }
+
+      while (j < scores.length) {
+        summScore = summScore + Number(scores[j++])
+      }
+      this.scores['minScore'] = Math.min.apply(null, scores)
+      this.scores['maxScore'] = Math.max.apply(null, scores)
+      this.notes['minNote'] = Math.min.apply(null, notes) * 10
+      this.notes['maxNote'] = Math.max.apply(null, notes) * 10
+      this.notes['moyenneNote'] = (summNotes / this.nb_participant) * 10
+      this.scores['moyenScore'] = summScore / this.nb_participant
+
+      for (let participation of success.participation_quiz) {
+        if (participation.reponse) {
+          for (let reponse of participation.reponse)
+            this.statistique_qst.push(reponse)
+        }
+      }
+
+      let res = []
+
+      for (let qst of this.table_question) {
+        let data = this.statistique_qst.filter((s: any) => s.qst == qst._id)
+
+        let nb_j = data.filter(
+          (d: any) => d.response.__valeur_reponse_juste == true,
+        ).length
+        let nb_f = data.filter(
+          (d: any) => d.response.__valeur_reponse_juste == false,
+        ).length
+        res.push({
+          qst: qst.translations['fr']?.__label_question,
+          nb_j: nb_j,
+          nb_f: nb_f,
+        })
+      }
+      this.statistique_qst = res
+    }
   }
 }
